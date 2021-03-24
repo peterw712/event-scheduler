feac6d5f39025dd8829edee4c0f01fdcbca23167
Peter Wang <65141087+peterw712@users.noreply.github.com>
2021-03-24 16:08:23 -0400

    set and get events in local storage

--- script.js
+++ script.js
@@ -1,3 +1,45 @@
+//display current date
+var currentDate = moment().format("dddd, MMMM Do");
+$("#currentDay").append(currentDate);
+// compare hour blocks with current hour and set color accordingly
+function setColor() {
+    var currentHour = moment().hours();
+    $(".description").each(function() {
+        var hourBlock = parseInt($(this).attr("id"));
+        if (hourBlock < currentHour) {
+            $(this).addClass("past");
+        }
+        else if (hourBlock === currentHour) {
+            $(this).addClass("present");
+        }
+        else {
+            $(this).addClass("future");
+        }
+    });
+}
+    
+setColor();
+//execute setColor every 10 minutes to keep up to date  
+setInterval(function () {
+    setColor();
+}, 600000);
+
+//set events in local storage  
+$(".saveBtn").click(function() {
+    var hour = $(this).siblings(".description").attr("id");
+    var event = $(this).siblings(".description").val();
+    localStorage.setItem(hour, event);
+});
+
+//get events in local storage
+
+function renderEvents() {
+    for (var i = 9; i < 18; i++) {
+        $("#" + [i]).val(localStorage.getItem([i]));
+    }
+}
+
+renderEvents();
 
 
 
